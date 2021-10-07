e62fa938042f34c8b39a844e74cbf304f1215518
myfriendpaul <pgonzalez069@gmail.com>
2021-10-07 10:58:04 -0700

    Setup client side base url for deployment

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://onlygoesup-api.herokuapp.com/"
+    : "http://localhost:3000";
 
 const baseUrl = "http://localhost:3000";
 
