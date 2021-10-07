500e68fcc526cea0d50dbef43b459f67fe225bf9
myfriendpaul <pgonzalez069@gmail.com>
2021-10-07 08:22:16 -0700

    User authentication on front end working and tested

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
+import "./App.css";
+
+import Layout from "./layouts/Layout";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
+// import MainContainer from './containers/MainContainer';
+
+import {
+  loginUser,
+  registerUser,
+  verifyUser,
+  removeToken,
+} from "./services/auth";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+    };
+    handleVerify();
+  }, []);
+
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData);
+    setCurrentUser(userData);
+    history.push("/");
+  };
+
+  const handleRegister = async (registerData) => {
+    const userData = await registerUser(registerData);
+    setCurrentUser(userData);
+    history.push("/");
+  };
+
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem("authToken");
+    removeToken();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout currentUser={currentUser} handleLogout={handleLogout}>
+        <Switch>
+          <Route path="/login">
+            <Login handleLogin={handleLogin} />
+          </Route>
+          <Route path="/register">
+            <Register handleRegister={handleRegister} />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
---
+++ client/src/layouts/Layout.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+export default function Layout(props) {
+  return (
+    <div>
+      <header>
+        <h1>Only Goes Up</h1>
+        {props.currentUser ? (
+          <div>
+            <p>{props.currentUser.name}</p>
+            <button onClick={props.handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <Link to="/register">Register</Link>
+        )}
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <hr />
+        {props.currentUser && (
+          <div>
+            <Link>Search Coins</Link>
+            <Link>Portfolio</Link>
+          </div>
+        )}
+      </header>
+      {props.children}
+    </div>
+  );
+}
